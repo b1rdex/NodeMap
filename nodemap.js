61c85d2443509a165770f62a93bdf58e0d0e7c79
Niki Liu <nishiki.liu@gmail.com>
2011-12-24 00:33:44 -0800

    Added attributes tooltips

--- nodemap.js
+++ nodemap.js
@@ -4,12 +4,11 @@
  * Website: http://about.me/nikiliu
  */
 
+// Get the root node of the page
+var rootNode = document.all[0];
+
 // The new page structure will be stored in this
 var mappedNodes = '';
-
-// Get the root node of the page, most of the time
-// it will be <html>
-var rootNode = document.all[0];
 
 // Map all the nodes in the root node recursively
 mapNodes(rootNode);
@@ -17,12 +16,25 @@ mapNodes(rootNode);
 /*
  * This function displays a node's tagName in a div
  * and repeats the process for every child element
- * recursively.
+ * recursively. New unordered lists are created for
+ * every child node of the given node.
  */
 function mapNodes(node) {
-    // Prevent text from being processed
+    // Prevent text nodes from being processed
     if (node.tagName != undefined) {
-        mappedNodes += '<div class="node">' + node.tagName + '</div>';
+        // Add an attributes div if the node has attributes
+        if (node.attributes.length > 0) {
+            var nodeAttributes = '';
+            for (var i = 0; i < node.attributes.length; i++) {
+                nodeAttributes += '<p>' + node.attributes[i].name + ': ' +
+                                  node.attributes[i].value + '</p>';
+            }
+            mappedNodes += '<div class="node">' + node.tagName +
+                           '<div class="attributes">' + nodeAttributes + '</div>' +
+                           '</div>';
+        } else {
+            mappedNodes += '<div class="node">' + node.tagName + '</div>';
+        }
     }
     
     // Run the function again for every child node
@@ -38,9 +50,7 @@ function mapNodes(node) {
 }
 
 // Clear the page's <head> tag to prevent stylesheet conflicts
-document.head.innerHTML = '<meta charset="utf-8" />';
+document.head.innerHTML = '';
 
 // Display the entire node map
-document.body.innerHTML = '<ul class="nodemap"><li>' +
-                          mappedNodes +
-                          '</li></ul>';
+document.body.innerHTML = '<ul class="nodemap"><li>' + mappedNodes + '</li></ul>';
